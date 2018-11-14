206f31efb8553c065be26c9b1ee7e65277fee8f0
Andrews <andrewschiozo@gmail.com>
2018-11-14 09:40:52 -0200

    Update base64download.php

--- base64download.php
+++ base64download.php
@@ -3,7 +3,7 @@
 if(array_key_exists('base64encoded', $_POST))
 {
 	$stringDecoded = base64_decode($_POST['base64encoded']);
-	$file = fopen('file.extension', 'w+');
+	$file = fopen('file.png', 'w+');
 	fwrite($file, $stringDecoded);
 	fclose($file);
 	echo json_encode('true');
